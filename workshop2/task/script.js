8e5ca68948d92ce2ada48605ad5aed0323aca613
bennypc <52959626+bennypc@users.noreply.github.com>
2024-09-25 00:00:05 -0700

    workshop

--- workshop2/task/script.js
+++ workshop2/task/script.js
@@ -1,1 +1,10 @@
 // WRITE YOUR SCRIPT HERE
+document
+  .getElementById("taskForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // GET VALUE
+
+    // PRINT VALUE
+  });
